1fb855585e6a8fa722571fb1f1c497e8a0179170
Camilo Villamizar <camilocodes@gmail.com>
2016-11-10 15:54:35 -0400

    Remove honorary_migration module

--- scripts/container/pre-init.d/90_enable_local_features.sh
+++ scripts/container/pre-init.d/90_enable_local_features.sh
@@ -2,7 +2,5 @@
 # Local alterations for your instance.
 # i.e. drush --root=${DRUPAL_ROOT} --uri=default --yes en thirty_two_project
 DRUSH_COMMAND="drush --root=${DRUPAL_ROOT} --uri=default --yes"
-$DRUSH_COMMAND en migrate_plus migrate_source_csv migrate_tools migrate_upgrade
+$DRUSH_COMMAND en migrate_plus migrate_tools
 $DRUSH_COMMAND cr
-$DRUSH_COMMAND en honorary_migration
-$DRUSH_COMMAND migrate-status
