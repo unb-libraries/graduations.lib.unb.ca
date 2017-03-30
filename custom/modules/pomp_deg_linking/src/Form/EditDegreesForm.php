5120c67cc827dc8a40c90e40e3122168a4ab66f6
Camilo Villamizar <camilocodes@gmail.com>
2017-03-30 14:59:34 -0300

    POMP-75 Add edit-degrees form - hopefully fixes build

---
+++ custom/modules/pomp_deg_linking/src/Form/EditDegreesForm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\pomp_deg_linking\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+use Drupal\views\Views;
+
+/**
+ * ManageProjectModulesForm object.
+ */
+class EditDegreesForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'pomp_deg_linking_edit_degrees_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $node = NULL) {
+    $form = [];
+  }
+
+}
