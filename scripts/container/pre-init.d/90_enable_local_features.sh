a0fc119b95aafa3d495168430a9b6c317482242d
Jacob Sanford <jsanford@unb.ca>
2017-05-10 14:46:40 -0300

    IN-25 Disable migration from inital deploy

--- scripts/container/pre-init.d/90_enable_local_features.sh
+++ scripts/container/pre-init.d/90_enable_local_features.sh
@@ -2,5 +2,4 @@
 # Local alterations for your instance.
 # i.e. drush --root=${DRUPAL_ROOT} --uri=default --yes en thirty_two_project
 DRUSH_COMMAND="drush --root=${DRUPAL_ROOT} --uri=default --yes"
-$DRUSH_COMMAND en migrate_plus migrate_tools
 $DRUSH_COMMAND cr
