9b81b7055ffa5f63a4a5169a2b1060a8ca856aad
Jacob Sanford <jsanford@unb.ca>
2017-05-19 13:07:04 -0300

    Update 90_enable_rsyslogd.sh

--- scripts/container/pre-init.d/90_enable_rsyslogd.sh
+++ scripts/container/pre-init.d/90_enable_rsyslogd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 if [ "$DEPLOY_ENV" == "prod" ]; then
   sed -i "s|LOGZIO_KEY|$LOGZIO_KEY|g" /etc/rsyslog.conf
-  /usr/sbin/rsyslogd -f /etc/rsyslog.conf
+  /usr/sbin/rsyslogd -f /etc/rsyslog.d/21-logzio-nginx.conf
 fi
