df8d9159649c42b8ee2a34eef0a7948870915b77
Jacob Sanford <jsanford@unb.ca>
2017-11-20 11:54:30 -0400

    Update visual regression reference images for dev

---
+++ tests/backstop/dev/backstop_data/html_report/config.js
@@ -0,0 +1,44 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20171120-155422/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_0_phone.png",
+        "label": "dev-graduations.lib.unb.ca Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20171120-155422/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_1_tablet.png",
+        "label": "dev-graduations.lib.unb.ca Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_2_monitor.png",
+        "test": "../bitmaps_test/20171120-155422/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_2_monitor.png",
+        "selector": "document",
+        "fileName": "graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_2_monitor.png",
+        "label": "dev-graduations.lib.unb.ca Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "error": "Reference file not found/src/backstop_data/bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_2_monitor.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
