857aa71e61163884e34dede366fb851cd4ca1e49
Camilo Villamizar <cvillami@unb.ca>
2018-02-22 12:41:04 -0400

    POMP-102 Add visual regression tests

--- tests/backstop/dev/backstop_data/html_report/config.js
+++ tests/backstop/dev/backstop_data/html_report/config.js
@@ -4,41 +4,68 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_0_phone.png",
-        "test": "../bitmaps_test/20171120-155422/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180222-163621/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_0_phone.png",
         "selector": "document",
         "fileName": "graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_0_phone.png",
         "label": "dev-graduations.lib.unb.ca Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "error": "Reference file not found/src/backstop_data/bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_0_phone.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.01",
+          "analysisTime": 87,
+          "getDiffImage": null
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_1_tablet.png",
-        "test": "../bitmaps_test/20171120-155422/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180222-163621/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_1_tablet.png",
         "selector": "document",
         "fileName": "graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_1_tablet.png",
         "label": "dev-graduations.lib.unb.ca Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "error": "Reference file not found/src/backstop_data/bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_1_tablet.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 223,
+          "getDiffImage": null
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_2_monitor.png",
-        "test": "../bitmaps_test/20171120-155422/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_2_monitor.png",
+        "test": "../bitmaps_test/20180222-163621/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_2_monitor.png",
         "selector": "document",
         "fileName": "graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_2_monitor.png",
         "label": "dev-graduations.lib.unb.ca Homepage",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
-        "error": "Reference file not found/src/backstop_data/bitmaps_reference/graduations_lib_unb_ca_dev-graduationslibunbca_Homepage_0_document_2_monitor.png"
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00",
+          "analysisTime": 341,
+          "getDiffImage": null
+        }
       },
-      "status": "fail"
+      "status": "pass"
     }
   ]
 });
