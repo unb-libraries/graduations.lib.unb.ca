0cd3f2016512346f3f9aae8d4aa353aa4acc2d16
camilocodes <camilocodes@gmail.com>
2021-01-15 11:56:29 -0400

    POMP-131 Node and script changes for v2 migration

---
+++ custom/modules/pomp_v2/scripts/rm_addresses.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Contains rm_addresses.php
+ * Remove new Address type data from graduations.lib.unb.ca v2.
+ */
+
+$query = \Drupal::entityQuery('node');
+$query->condition('type', 'address');
+$tids = $query->execute();
+
+$storage_handler = \Drupal::entityTypeManager()->getStorage($entity_type);
+$entities = $storage_handler->loadMultiple($tids);
+$storage_handler->delete($entities);
