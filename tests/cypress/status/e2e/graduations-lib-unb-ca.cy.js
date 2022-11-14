83d0b4682ac302905f7891bd48c7410413ccd2c7
patschilf <46682967+patschilf@users.noreply.github.com>
2022-11-14 10:33:06 -0400

    Explicitly block requests to UNB's fontkit

--- tests/cypress/status/e2e/graduations-lib-unb-ca.cy.js
+++ tests/cypress/status/e2e/graduations-lib-unb-ca.cy.js
@@ -1,5 +1,9 @@
 const host = 'https://graduations.lib.unb.ca'
 describe('Graduations', {baseUrl: host, groups: ['sites']}, () => {
+  
+  beforeEach(() => {
+    cy.blockUnbFont()
+  })
 
   context('Front page', {baseUrl: host}, () => {
     beforeEach(() => {
